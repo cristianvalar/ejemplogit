0f4bbfffe1edbc584afe321cdf8dd27aa0e7cb76
Ivan Quintanilla <imqa316@gmail.com>
2013-05-16 17:56:29 -0400

    El saludo se incluye en la pagina

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,6 @@
 $(window).on('ready', function(){
 	$('#saludo').on('click', function(){
 		var nombre = $('input').val();
-		alert('hola '+nombre);
+		$('#parrafo').append("<h2>Hola "+ nombre +"</h2>");
 	});
 });
